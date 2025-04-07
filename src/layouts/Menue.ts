bed767e61efe53cfbc923cfc7b4f70e10d91dcd6
Florian Reisinger <reisi007@gmail.com>
2025-04-07 19:14:43 +0200

    Improve page

--- src/layouts/Menue.ts
+++ src/layouts/Menue.ts
@@ -1,31 +1,8 @@
-export type Menue = (MenueItem | SubMenu)[];
-
-export enum Visibility {
-    ALWAYS,
-    DEV_ONLY,
-    PROD_ONLY,
-}
+export type Menue = MenueItem [];
 
 export type MenueItem = {
-    href: string,
-    name: string,
-    visibility?: Visibility,
-
-}
-
-export type SubMenu = {
-    name: string,
-    baseHref: string,
-    items: MenueItem[],
-    visibility?: Visibility,
-}
-
-export function isSubMenu(item: MenueItem | SubMenu): item is SubMenu {
-    return (item as SubMenu).baseHref !== undefined;
-}
-
-export function isMenuItem(item: MenueItem | SubMenu): item is MenueItem {
-    return !isSubMenu(item);
+  href: string,
+  name: string,
 }
 
 
