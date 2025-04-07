4064b94bc72d13692230548a828e21745f85f743
Florian Reisinger <reisi007@gmail.com>
2025-04-07 20:49:42 +0200

    Re introduce menu

--- src/layouts/Menue.ts
+++ src/layouts/Menue.ts
@@ -1,8 +1,27 @@
-export type Menue = MenueItem [];
+export type Menue = (MenueItem | SubMenu)[];
+
+export enum Visibility {
+  ALWAYS,
+  DEV_ONLY,
+  PROD_ONLY,
+}
 
 export type MenueItem = {
   href: string,
   name: string,
+  visibility?: Visibility,
+
+}
+
+export type SubMenu = {
+  name: string,
+  baseHref: string,
+  items: MenueItem[],
+  visibility?: Visibility,
+}
+
+export function isSubMenu(item: MenueItem | SubMenu): item is SubMenu {
+  return (item as SubMenu).baseHref !== undefined;
 }
 
 
