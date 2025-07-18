eaf60c2347338a1e8ea91688786c8721b60eb2e1
Florian Reisinger <reisi007@gmail.com>
2025-07-18 21:21:43 +0200

    Add Matomo integration, update styles, and refactor components
    - Integrated `astro-matomo` for analytics with necessary configuration updates in `astro.config.mjs`.
    - Updated global styles for better print and accessibility support.
    - Refactored layout components to enhance design consistency and usability.
    - Simplified `ResponsiveImage` by removing background image logic.
    - Added `.htaccess` for URL rewrite in the `einblicke` directory.
    - Updated `package.json` and `package-lock.json` to reflect new dependencies.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
-import {defineConfig} from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import solidJs from "@astrojs/solid-js";
 import tailwindcss from "@tailwindcss/vite";
+import matomo from "astro-matomo";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://reisinger.pictures',
-    cacheDir: "./.cache",
-    vite: {plugins: [tailwindcss()]},
-    integrations: [mdx(), sitemap(), solidJs()]
+  site: "https://reisinger.pictures",
+  cacheDir: "./.cache",
+  vite: { plugins: [tailwindcss()] },
+  integrations: [mdx(), sitemap(), solidJs(), matomo({
+    enabled: import.meta.env.PROD,
+    host: "https://analytics.reisinger.pictures/",
+    siteId: 1,
+    viewTransition: true
+  })]
 });
