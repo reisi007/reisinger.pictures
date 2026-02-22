c716a5736da5c2efb9490be6d13b843a725f10ff
Florian Reisinger <reisi007@gmail.com>
2026-02-22 21:40:22 +0100

    feat(config): Exclude specific pages from sitemap generation
    - Added `excludedPages` array to define paths to exclude from the sitemap.
    - Updated sitemap integration in `astro.config.mjs` to filter out excluded pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,18 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
+const excludedPages = [
+  "preise/ogs/"
+];
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://reisinger.pictures",
   cacheDir: "./.cache",
   vite: { plugins: [tailwindcss()] },
-  integrations: [mdx(), sitemap()]
+  integrations: [mdx(), sitemap({
+    filter(page) {
+      return !excludedPages.some(pagePath => page.endsWith(pagePath));
+    }
+  })]
 });
