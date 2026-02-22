987a96d8b0a23aa3c90d89624378501166612774
Florian Reisinger <reisi007@gmail.com>
2026-02-22 20:17:08 +0100

    chore(sync, layouts): Increase `rclone` transfers and clean up unused import in `Header.astro`

--- sync.bat
+++ sync.bat
@@ -1,1 +1,1 @@
-rclone sync E:\astro-reisinger.pictures\dist reisinger.pictures:/reisinger.pictures --transfers=64 --track-renames --progress
+rclone sync E:\astro-reisinger.pictures\dist reisinger.pictures:/reisinger.pictures --transfers=128 --track-renames --progress
